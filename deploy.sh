09bb8131886dd6631934ba4acfc4299851ac9524
Graham Pentheny <graham.pentheny@gmail.com>
2018-12-05 16:29:47 -0500

    deploy script ignores .DS_Store

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # depends on the aws cli http://aws.amazon.com/cli/
-aws s3 sync bucket/. s3://spryfox-alphabear2 --acl=public-read
+aws s3 sync bucket/. s3://spryfox-alphabear2 --exclude "*.DS_Store*" --acl=public-read
